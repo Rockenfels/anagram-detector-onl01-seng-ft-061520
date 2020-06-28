aa5b0a73b62ff2d4695a07536360a372f9ecbaf9
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-28 03:31:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagrams)
+    word = @word.split("")
+    matches = []
+    
+    anagrams.each do |anagram|
+      test = anagram.split("")
+      i = 0
+      match = true
+      
+      if word.length == test.length
+        while i < anagram.length do
+          if word[i].include?(test[i]) == false
+            match = false
+          end
+          i += 1
+        end
+      end
+      
+      if match == true
+        matches << anagram
+      end
+    end
+    matches
+  end
+  
+end
